033cf4e38c1e66948383ebfcb28be348499cf92e
bliaowey <bliao@brandeis.edu>
2022-10-05 19:58:19 -0400

    Added basic seeding

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby "3.1.2"
 gem "rails", "~> 7.0.4"
 gem "bootstrap-sass",  "3.4.1"
 
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'seeding'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+
+users = []
+for i in 0..50 do
+    n = Faker::Name.name
+    e = Faker::Internet.email
+    a = Faker::Number.number(digits: 2)
+    id = Faker::Number.number(digits: 3)
+
+    b = [n, e, a, id]
+    users[i] = b
+end
+
+
+posts = []
+for i in 0..50 do
+    uid = Faker::Number.number(digits: 3) #user id
+    pid = Faker::Number.number(digits: 3) #post id
+    rid = Faker::Number.number(digits: 3) #restaurant id
+    rating = Faker::Number.number(digits: 1)
+    likes = Faker::Number.number(digits: 3)
+
+    b = [pid, uid, rid, rating, likes]
+    posts[i] = b
+end
+
+#print posts
+
+comments = []
+for i in 0..50 do
+    uid = Faker::Number.number(digits: 3)
+    pid = Faker::Number.number(digits: 3)
+    comment = Faker::Restaurant.review
+
+    b = [pid, uid, comment]
+    comments[i] = b
+end
+
+#print comments
+
+restaurants = []
+for i in 0..50 do
+    rid = Faker::Number.number(digits: 3)
+    name = Faker::Restaurant.name
+    location = Faker::WorldCup.city
+    lower_range = Faker::Number.number(digits: 2)
+    upper_range = Faker::Number.number(digits: 3)
+
+    b = [rid, name, location, lower_range, upper_range]
+    restaurants[i] = b
+end
+
+print restaurants
