729072faefb7bd72fbdf7a4a8f991d2e63416203
MahmoudSalah <mahmouds343@gmail.com>
2022-10-05 19:54:19 -0400

    added a resource for posts and created relationships between users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_232112) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_233014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.text "content"
+    t.integer "rating"
+    t.integer "likes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
