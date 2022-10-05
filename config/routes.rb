633262f655d79fc829141fb1b8e36af28abb7998
Aarti-Jain <aartijain@brandeis.edu>
2022-10-05 19:48:19 -0400

    made the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'users#index'
+  # root 'users#index'
+  root "static_pages#home"
+  get  "/signup",  to: "users#new"
+  get  "/about",   to: "static_pages#about"
 end
