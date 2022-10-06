61d5571a86f9e804de830a5f5ba73dcc770b0a37
MahmoudSalah <mahmouds343@gmail.com>
2022-10-06 17:46:42 -0400

    added tests and validations for comments and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :posts
+
+  validates :id, presence: true, uniqueness: true
+  validates :name, presence: true, length: {maximum: 100}
+  validates :location, presence: true, length: {maximum: 100}
+  validates :price_range, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = Comment.new(post_id:1, user_id:1, content:"content")
+  end
+
+  test "name should be present" do
+    @comment.content = "        "
+    assert_not @comment.valid?
+  end
+
+  test "post id should be present" do
+    @comment.post_id = "        "
+    assert_not @comment.valid?
+  end
+
+  test "user id should be present" do
+    @comment.user_id = "             "
+    assert_not @comment.valid?
+  end
+
+  test "content should not be too long" do
+    @comment.content = "a" * 20000
+    assert_not @comment.valid?
+  end
 end
