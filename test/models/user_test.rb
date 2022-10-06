32b0cf369cc6a47670ff758c914e104c87ec9cbf
Aarti-Jain <aartijain@brandeis.edu>
2022-10-06 17:22:34 -0400

    tests for the user version 0.1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user should have a name" do
+    @user = User.new(name: " ", email: "testuser@example.com", age: 21, bio: "example bio")
+    assert_equal(false,@user.save)
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: nil 21, bio: "example bio")
+    assert_equal(true,@user.save)
+  end
+
+  test "user email has to be present" do
+    @user = User.new(name: "testUser", email:" ", age: 21, bio: "example bio")
+    assert_equal(false,@user.save)
+
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: 21, bio: "example bio")
+    assert_equal(true,@user.save)
+  end
+
+  test "user email has to be unique" do
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: 21, bio: "example bio")
+    @user2 = User.new(name: "testUser2", email:"testuser@example.com", age: 21, bio: "example bio 2")
+    assert_equal(true,@user.save)
+    assert_equal(false,@user2.save)
+
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: 21, bio: "example bio")
+    @user2 = User.new(name: "testUser2", email:"testuser2@example.com", age: 21, bio: "example bio 2")
+    assert_equal(true,@user.save)
+    assert_equal(true,@user2.save)
+
+  end 
+
+  test "user age has to be present" do
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: , bio: "example bio")
+    assert_equal(false,@user.save)
+
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: 22, bio: "example bio")
+    assert_equal(true,@user.save)
+  end
+
+  test "user bio has to be present" do
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: , bio: " ")
+    assert_equal(false,@user.save)
+
+    @user = User.new(name: "testUser", email:"testuser@example.com", age: 22, bio: "example bio")
+    assert_equal(true,@user.save)
+
+  end
 end
