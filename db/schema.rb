1e25e8accbc16effec8f0311d27b9506eda55046
MahmoudSalah <mahmouds343@gmail.com>
2022-10-06 16:42:15 -0400

    added a resource for posts and restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_233014) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_202342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +28,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_233014) do
     t.text "content"
     t.integer "rating"
     t.integer "likes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "location"
+    t.integer "price_range"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
